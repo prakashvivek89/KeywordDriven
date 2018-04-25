4ff5449b4b4508596b59b8dfaad3b32620d25f44
prakashvivek89 <prakash.vivek91@yahoo.com>
2018-04-25 13:35:49 +0530

    Delete getAdviceEventAddChargeInfoByClientReference.sql

--- SQLQueries/getAdviceEventAddChargeInfoByClientReference.sql
+++
@@ -1,7 +0,0 @@
-select convert(varchar,cast(SUM(AdviceCharge.AmountDue) as money),1) amountDue, IntAdviceEventStatus.Description statusDes, CodeAdviceEventType.Description eventDes
- from AdviceEvent 
-inner join AdviceCharge on AdviceCharge.AdviceEventID = AdviceEvent.ID 
-inner join IntAdviceEventStatus on AdviceEvent.StatusID = IntAdviceEventStatus.ID 
-inner join CodeAdviceEventType on AdviceEvent.TypeID = CodeAdviceEventType.ID
-where AdviceEvent.ClientID = '{argument}'
-group by AdviceCharge.AdviceEventID, IntAdviceEventStatus.Description , CodeAdviceEventType.Description 
